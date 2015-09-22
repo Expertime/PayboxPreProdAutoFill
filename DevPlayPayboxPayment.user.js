587e8a06b074f9fcf4e75addc924a1476a902250
Matthieu Tudury <mtudury@expertime.com>
2015-09-22 14:24:23 +0200

    For dev purpose, inject PlayPaybox IPN from Magento Admin

---
+++ DevPlayPayboxPayment.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Magento Inject IPN from Admin (DEV!)
+// @namespace    http://www.expertime.com/
+// @version      0.1
+// @description  Magento inject IPN from admin page (DEV!)
+// @author       M.TUDURY
+// @match        http://*.dev/index.php/admin/sales_order/view/order_id/*/key/*/
+// @grant        unsafeWindow
+// @grant        GM_log
+// ==/UserScript==
+
+    // test de mise a jour !
+(function () {
+    var $;
+
+    // Add jQuery
+    (function(){
+        if (typeof unsafeWindow.jQuery == 'undefined') {
+            var GM_Head = document.getElementsByTagName('head')[0] || document.documentElement,
+                GM_JQ = document.createElement('script');
+    
+            GM_JQ.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';
+            GM_JQ.type = 'text/javascript';
+            GM_JQ.async = true;
+    
+            GM_Head.insertBefore(GM_JQ, GM_Head.firstChild);
+        }
+        GM_wait();
+    })();
+
+    // Check if jQuery's loaded
+    function GM_wait() {
+        if (typeof unsafeWindow.jQuery == 'undefined') {
+            window.setTimeout(GM_wait, 100);
+        } else {
+            $ = unsafeWindow.jQuery.noConflict(true);
+            letsJQuery();
+        }
+    }
+    
+    
+    function letsJQuery() {
+        GM_log($().jquery); // check jQuery version
+        $('.content-header .form-buttons').prepend( "<button id=\"payboxpaymentdo\" title=\"PlayPayboxPayment\" type=\"button\" class=\"scalable\"><span><span><span>PlayPayboxPayment</span></span></span></button>" );
+    }
+    
+})();
